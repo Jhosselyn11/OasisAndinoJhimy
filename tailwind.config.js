18684aaa1c126657691538504cbb55ed420d3670
jying08 <chance.jhimy@gmail.com>
2024-05-03 12:02:24 -0500

    termiando una parte

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,47 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'primary':"#657A38",
+        primary: {"50":"#eff6ff","100":"#dbeafe","200":"#bfdbfe","300":"#93c5fd","400":"#60a5fa","500":"#3b82f6","600":"#2563eb","700":"#1d4ed8","800":"#1e40af","900":"#1e3a8a","950":"#172554"},
+        'primaryy':"#657A38",
         'secondary':"#8FA206",
         'tertiary':"#61AEC9",
       },
       fontFamily: {
+        'body': [
+          'Inter', 
+          'ui-sans-serif', 
+          'system-ui', 
+          '-apple-system', 
+          'system-ui', 
+          'Segoe UI', 
+          'Roboto', 
+          'Helvetica Neue', 
+          'Arial', 
+          'Noto Sans', 
+          'sans-serif', 
+          'Apple Color Emoji', 
+          'Segoe UI Emoji', 
+          'Segoe UI Symbol', 
+          'Noto Color Emoji'
+        ],
         Montserrat: ["Montserrat", "sans-serif"],
+        'sans': [
+          'Inter', 
+          'ui-sans-serif', 
+          'system-ui', 
+          '-apple-system', 
+          'system-ui', 
+          'Segoe UI', 
+          'Roboto', 
+          'Helvetica Neue', 
+          'Arial', 
+          'Noto Sans', 
+          'sans-serif', 
+          'Apple Color Emoji', 
+          'Segoe UI Emoji', 
+          'Segoe UI Symbol', 
+          'Noto Color Emoji'
+        ],
       },
       backgroundImage: {
         'sanFrancisco': "url('./img/sanFrancisco.jpg')",
@@ -30,7 +65,7 @@ module.exports = {
 			  'iceland': "url('/src/img/iceland.jpg')",
       },
       fontSize:{
-        'xl-plus': '55px', // Personaliza el tamaño del texto a 1.25rem
+        'xl-plus': '75px', // Personaliza el tamaño del texto a 1.25rem
 
       },
       height:{
